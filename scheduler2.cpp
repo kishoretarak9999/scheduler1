77372913addc3e4296c5efcd5f74516be3794e3a
kishoretarak9999 <49436526+kishoretarak9999@users.noreply.github.com>
2019-04-15 17:23:25 +0530

    Add files via upload

---
+++ scheduler2.cpp
@@ -0,0 +1,52 @@
+#include<stdio.h>
+ int main()
+{
+    int bt[20],p[20],wt[20],tat[20],i,j,n,total=0,pos,temp;
+    float avg_wt,avg_tat;
+    printf("Enter number of process:");
+    scanf("%d",&n);
+ 
+    printf("\nEnter Arrival Time:\n");
+    for(i=0;i<n;i++)
+    {
+        printf("p%d:",i+1);
+        scanf("%d",&bt[i]);
+        p[i]=i+1;           //contains process number
+    }
+printf("Burst time of the process will be double the Arrival time.\n") ;
+printf("process number\tArrival time\tBurst time\n");
+for(i=0;i<n;i++)
+{
+printf("P%d\t\t%d\t\t%d\n",i+1,bt[i],bt[i]*2);
+}
+    //sorting burst time in ascending order using selection sort
+    for(i=0;i<n;i++)
+    {
+        pos=i;
+        for(j=i+1;j<n;j++)
+        {
+            if(bt[j]>bt[pos])
+                pos=j;
+        }
+ 
+        temp=bt[i];
+        bt[i]=bt[pos];
+        bt[pos]=temp;
+ 
+        temp=p[i];
+        p[i]=p[pos];
+        p[pos]=temp;
+    }
+ 
+    wt[0]=0;            //waiting time for first process will be zero
+ 
+    //calculate waiting time
+    for(i=1;i<n;i++)
+    {
+        wt[i]=0;
+        for(j=0;j<i;j++)
+            wt[i]+=bt[j]*2;
+ 
+        total+=wt[i];
+    }
+}
